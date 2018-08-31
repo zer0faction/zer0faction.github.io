ab72ed25daa44b5ea06be9661f07721ecb0548d1
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-08-31 16:06:36 +0200

    Enge veranderingen 33

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
- * See https://goo.gl/S9QRabhhh
+ * See https://goo.gl/S9QRabhhhssssssss
  */
 self.__precacheManifest = [
   {
