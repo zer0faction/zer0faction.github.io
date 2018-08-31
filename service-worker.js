c985c7538602b20278be50f6da1549fe5323792f
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-08-31 11:44:29 +0200

    Enge veranderingen 7

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
-console.log('Deze log komt uit de service worker');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Workbox is loaded`);
+} else {
+  console.log(`Workbox didn't load`);
+}
