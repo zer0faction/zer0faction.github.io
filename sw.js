340306a4688cda56c65472f0ea8c7b1b0177d875
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-09-03 14:33:31 +0200

    Enge veranderingen 66

--- sw.js
+++ sw.js
@@ -1,52 +1,40 @@
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('v1').then(function (cache) {
-      return cache.addAll([
-        '/src/App.vue',
-        '/src/main.js',
-        '/src/assets/logo.png',
-        '/index.html',
-        '/public/img'
-      ])
-    })
-  )
-})
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
-// /**
-//  * Welcome to your Workbox-powered service worker!
-//  *
-//  * You'll need to register this file in your web app and you should
-//  * disable HTTP caching for this file too.
-//  * See https://goo.gl/nhQhGp
-//  *
-//  * The rest of the code is auto-generated. Please don't update this file
-//  * directly; instead, make changes to your Workbox build configuration
-//  * and re-run your build process.
-//  * See https://goo.gl/2aRDsh
-//  */
-//
-// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
-//
-// /**
-//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
-//  * requests for URLs in the manifest.
-//  * See https://goo.gl/S9QRabhhhssssssss
-//  */
-// self.__precacheManifest = [
-//   {
-//     "url": "src/App.vue",
-//     "revision": "618d875d9f3e2ea01243a888c23e5fc6"
-//   },
-//   {
-//     "url": "src/assets/logo.png",
-//     "revision": "82b9c7a5a3f405032b1db71a25f67021"
-//   },
-//   {
-//     "url": "main.js",
-//     "revision": "3e1d05bd9260c62c829837e16c61e54f"
-//   }
-// ].concat(self.__precacheManifest || []);
-// workbox.precaching.suppressWarnings();
-// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-//
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRabhhhssssssss
+ */
+self.__precacheManifest = [
+  {
+    "url": "src/App.vue",
+    "revision": "618d875d9f3e2ea01243a888c23e5fc6"
+  },
+  {
+    "url": "src/assets/logo.png",
+    "revision": "82b9c7a5a3f405032b1db71a25f67021"
+  },
+  {
+    "url": "src/main.js",
+    "revision": "3e1d05bd9260c62c829837e16c61e54f"
+  },
+  {
+
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
