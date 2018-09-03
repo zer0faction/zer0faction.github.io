dd61098a0a7b786fe2c07b0fc6167dc250fbd55e
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-09-03 15:49:07 +0200

    Enge veranderingen 76

--- sw.js
+++ sw.js
@@ -1,27 +1,103 @@
-// /**
-//  * Welcome to your Workbox-powered service worker!
-//  *
-//  * You'll need to register this file in your web app and you should
-//  * disable HTTP caching for this file too.
-//  * See https://goo.gl/nhQhGp
-//  *
-//  * The rest of the code is auto-generated. Please don't update this file
-//  * directly; instead, make changes to your Workbox build configuration
-//  * and re-run your build process.
-//  * See https://goo.gl/2aRDsh
-//  */
-//
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
-//
-// /**
-//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
-//  * requests for URLs in the manifest.fffffff
-//  * See https://goo.gl/S9QRabhhhssssssssgggg
-//  */
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
 self.__precacheManifest = [
   {
+    "url": "dist/build.js",
+    "revision": "123de730716c8e828e3d1a09c8b009a2"
+  },
+  {
+    "url": "dist/index.html",
+    "revision": "f0f373ed56ef95c071e1fa753d9ebbfe"
+  },
+  {
+    "url": "index.html",
+    "revision": "3fc880913b92e43db961c50a74fae21c"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "b573741dd3cf0dae51660a7454632ecd"
+  },
+  {
+    "url": "public/img/icons/android-chrome-192x192.png",
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4"
+  },
+  {
+    "url": "public/img/icons/android-chrome-512x512.png",
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814"
+  },
+  {
+    "url": "public/img/icons/apple-touch-icon-120x120.png",
+    "revision": "936d6e411cabd71f0e627011c3f18fe2"
+  },
+  {
+    "url": "public/img/icons/apple-touch-icon-152x152.png",
+    "revision": "1a034e64d80905128113e5272a5ab95e"
+  },
+  {
+    "url": "public/img/icons/apple-touch-icon-180x180.png",
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51"
+  },
+  {
+    "url": "public/img/icons/apple-touch-icon-60x60.png",
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db"
+  },
+  {
+    "url": "public/img/icons/apple-touch-icon-76x76.png",
+    "revision": "af28d69d59284dd202aa55e57227b11b"
+  },
+  {
+    "url": "public/img/icons/apple-touch-icon.png",
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e"
+  },
+  {
+    "url": "public/img/icons/favicon-16x16.png",
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3"
+  },
+  {
+    "url": "public/img/icons/favicon-32x32.png",
+    "revision": "98b614336d9a12cb3f7bedb001da6fca"
+  },
+  {
+    "url": "public/img/icons/msapplication-icon-144x144.png",
+    "revision": "b89032a4a5a1879f30ba05a13947f26f"
+  },
+  {
+    "url": "public/img/icons/mstile-150x150.png",
+    "revision": "058a3335d15a3eb84e7ae3707ba09620"
+  },
+  {
+    "url": "public/img/icons/safari-pinned-tab.svg",
+    "revision": "f22d501a35a87d9f21701cb031f6ea17"
+  },
+  {
+    "url": "public/robots.txt",
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca"
+  },
+  {
+    "url": "README.md",
+    "revision": "90d1fbdbb2a9d98d5575c58d61bc10cf"
+  },
+  {
     "url": "src/App.vue",
-    "revision": "618d875d9f3e2ea01243a888c23e5fc6"
+    "revision": "8c0984d97eed0e7c90cf85a5db748423"
   },
   {
     "url": "src/assets/logo.png",
@@ -29,38 +105,20 @@ self.__precacheManifest = [
   },
   {
     "url": "src/main.js",
-    "revision": "3e1d05bd9260c62c829837e16c61e54f"
+    "revision": "fc567c18696cf9625371528474a889ce"
+  },
+  {
+    "url": "src/registerServiceWorker.js",
+    "revision": "95cc3ee3452d555b9e807746a497850a"
+  },
+  {
+    "url": "webpack.config.js",
+    "revision": "eb4d99ba01c1a7ea50568dd96412e8d3"
+  },
+  {
+    "url": "workbox-config.js",
+    "revision": "f55dad84f5b40d768b12a8c0c96a9103"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
-
-
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.networkFirst()
-)
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.vue'),
-  workbox.strategies.networkFirst()
-)
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.png'),
-  workbox.strategies.networkFirst()
-)
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.html'),
-  workbox.strategies.networkFirst()
-)
-
--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "globDirectory": "src/",
-  "globPatterns": [
-    "**/*.{vue,png,js}"
-  ],
-  "swDest": "src\\sw.js"
-};
