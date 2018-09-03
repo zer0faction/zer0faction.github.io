ec8f4070c7fe8261d33fb16cf47a4f7049c1a3f3
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-09-03 14:59:59 +0200

    Enge veranderingen 70

--- sw.js
+++ sw.js
@@ -1,37 +1,66 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+// /**
+//  * Welcome to your Workbox-powered service worker!
+//  *
+//  * You'll need to register this file in your web app and you should
+//  * disable HTTP caching for this file too.
+//  * See https://goo.gl/nhQhGp
+//  *
+//  * The rest of the code is auto-generated. Please don't update this file
+//  * directly; instead, make changes to your Workbox build configuration
+//  * and re-run your build process.
+//  * See https://goo.gl/2aRDsh
+//  */
+//
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+//
+// /**
+//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+//  * requests for URLs in the manifest.fffffff
+//  * See https://goo.gl/S9QRabhhhssssssss
+//  */
+// self.__precacheManifest = [
+//   {
+//     "url": "src/App.vue",
+//     "revision": "618d875d9f3e2ea01243a888c23e5fc6"
+//   },
+//   {
+//     "url": "src/assets/logo.png",
+//     "revision": "82b9c7a5a3f405032b1db71a25f67021"
+//   },
+//   {
+//     "url": "src/main.js",
+//     "revision": "3e1d05bd9260c62c829837e16c61e54f"
+//   }
+// ].concat(self.__precacheManifest || []);
+// workbox.precaching.suppressWarnings();
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+// workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+//
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.fffffff
- * See https://goo.gl/S9QRabhhhssssssss
- */
-self.__precacheManifest = [
-  {
-    "url": "src/App.vue",
-    "revision": "618d875d9f3e2ea01243a888c23e5fc6"
-  },
-  {
-    "url": "src/assets/logo.png",
-    "revision": "82b9c7a5a3f405032b1db71a25f67021"
-  },
-  {
-    "url": "src/main.js",
-    "revision": "3e1d05bd9260c62c829837e16c61e54f"
-  }
-].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.networkFirst()
+)
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.vue'),
+  workbox.strategies.networkFirst()
+)
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.png'),
+  workbox.strategies.networkFirst()
+)
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.html'),
+  workbox.strategies.networkFirst()
+)
 
