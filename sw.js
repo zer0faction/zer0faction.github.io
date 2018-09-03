899f11c4f7921261f2a69ef0da9b76fbb4c2dbc2
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-09-03 15:35:00 +0200

    Enge veranderingen 73

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 //  * See https://goo.gl/2aRDsh
 //  */
 //
-// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 //
 // /**
 //  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -37,7 +37,7 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 } else {
