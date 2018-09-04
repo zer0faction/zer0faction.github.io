24361f83033dc9f15ada5574e2db8d96323e6b55
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-09-04 11:01:19 +0200

    Enge veranderingen 97: werkt lokaal alleen

--- sw.js
+++ sw.js
@@ -25,107 +25,107 @@ if (workbox) {
  */
 self.__precacheManifest = [
   {
-    "url": "dist/build.js",
+    "url": "./dist/build.js",
     "revision": "123de730716c8e828e3d1a09c8b009a2"
   },
   {
-    "url": "dist/index.html",
+    "url": "./dist/index.html",
     "revision": "f0f373ed56ef95c071e1fa753d9ebbfe"
   },
   {
-    "url": "index.html",
+    "url": "./index.html",
     "revision": "3fc880913b92e43db961c50a74fae21c"
   },
   {
-    "url": "manifest.json",
+    "url": "./manifest.json",
     "revision": "cf3d7514012f169d99186cc18ace9564"
   },
   {
-    "url": "package-lock.json",
+    "url": "./package-lock.json",
     "revision": "dd896f8ff9c94a23713aa3fbe3c560ea"
   },
   {
-    "url": "package.json",
+    "url": "./package.json",
     "revision": "d9b1e48e8e423c3d34d0d7126afa5a40"
   },
   {
-    "url": "public/img/icons/android-chrome-192x192.png",
+    "url": "./public/img/icons/android-chrome-192x192.png",
     "revision": "f130a0b70e386170cf6f011c0ca8c4f4"
   },
   {
-    "url": "public/img/icons/android-chrome-512x512.png",
+    "url": "./public/img/icons/android-chrome-512x512.png",
     "revision": "0ff1bc4d14e5c9abcacba7c600d97814"
   },
   {
-    "url": "public/img/icons/apple-touch-icon-120x120.png",
+    "url": "./public/img/icons/apple-touch-icon-120x120.png",
     "revision": "936d6e411cabd71f0e627011c3f18fe2"
   },
   {
-    "url": "public/img/icons/apple-touch-icon-152x152.png",
+    "url": "./public/img/icons/apple-touch-icon-152x152.png",
     "revision": "1a034e64d80905128113e5272a5ab95e"
   },
   {
-    "url": "public/img/icons/apple-touch-icon-180x180.png",
+    "url": "./public/img/icons/apple-touch-icon-180x180.png",
     "revision": "c43cd371a49ee4ca17ab3a60e72bdd51"
   },
   {
-    "url": "public/img/icons/apple-touch-icon-60x60.png",
+    "url": "./public/img/icons/apple-touch-icon-60x60.png",
     "revision": "9a2b5c0f19de617685b7b5b42464e7db"
   },
   {
-    "url": "public/img/icons/apple-touch-icon-76x76.png",
+    "url": "./public/img/icons/apple-touch-icon-76x76.png",
     "revision": "af28d69d59284dd202aa55e57227b11b"
   },
   {
-    "url": "public/img/icons/apple-touch-icon.png",
+    "url": "./public/img/icons/apple-touch-icon.png",
     "revision": "66830ea6be8e7e94fb55df9f7b778f2e"
   },
   {
-    "url": "public/img/icons/favicon-16x16.png",
+    "url": "./public/img/icons/favicon-16x16.png",
     "revision": "4bb1a55479d61843b89a2fdafa7849b3"
   },
   {
-    "url": "public/img/icons/favicon-32x32.png",
+    "url": "./public/img/icons/favicon-32x32.png",
     "revision": "98b614336d9a12cb3f7bedb001da6fca"
   },
   {
-    "url": "public/img/icons/msapplication-icon-144x144.png",
+    "url": "./public/img/icons/msapplication-icon-144x144.png",
     "revision": "b89032a4a5a1879f30ba05a13947f26f"
   },
   {
-    "url": "public/img/icons/mstile-150x150.png",
+    "url": "./public/img/icons/mstile-150x150.png",
     "revision": "058a3335d15a3eb84e7ae3707ba09620"
   },
   {
-    "url": "public/img/icons/safari-pinned-tab.svg",
+    "url": "./public/img/icons/safari-pinned-tab.svg",
     "revision": "f22d501a35a87d9f21701cb031f6ea17"
   },
   {
-    "url": "public/robots.txt",
+    "url": "./public/robots.txt",
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca"
   },
   {
-    "url": "README.md",
+    "url": "./README.md",
     "revision": "90d1fbdbb2a9d98d5575c58d61bc10cf"
   },
   {
-    "url": "src/App.vue",
+    "url": "./src/App.vue",
     "revision": "8c0984d97eed0e7c90cf85a5db748423"
   },
   {
-    "url": "src/assets/logo.png",
+    "url": "./src/assets/logo.png",
     "revision": "82b9c7a5a3f405032b1db71a25f67021"
   },
   {
-    "url": "src/main.js",
+    "url": "./src/main.js",
     "revision": "fc567c18696cf9625371528474a889ce"
   },
   {
-    "url": "src/registerServiceWorker.js",
+    "url": "./src/registerServiceWorker.js",
     "revision": "95cc3ee3452d555b9e807746a497850a"
   },
   {
-    "url": "webpack.config.js",
+    "url": "./webpack.config.js",
     "revision": "eb4d99ba01c1a7ea50568dd96412e8d3"
   }
 ].concat(self.__precacheManifest || []);
