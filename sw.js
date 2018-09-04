24a9eaf43df193f6761ef6b62eae082ea11c7445
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-09-04 08:58:19 +0200

    Enge veranderingen 84

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Welcome to your Workbox-powered service worker!
  *
  * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
+ * disable HTTP caching for this fildde too.
  * See https://goo.gl/nhQhGp
  *
  * The rest of the code is auto-generated. Please don't update this file
