8207df0433b8aba839a3f300b6254c1e9e0f7e40
erin.wallaard@outlook.com <erin.wallaard@outlook.com>
2018-09-04 10:54:48 +0200

    Enge veranderingen 96: werkt lokaal alleen

--- sw.js
+++ sw.js
@@ -12,6 +12,11 @@
  */
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
